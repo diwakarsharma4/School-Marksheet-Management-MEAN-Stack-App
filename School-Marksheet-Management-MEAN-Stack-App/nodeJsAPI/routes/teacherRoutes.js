ee4c9d8429e1eeac09cfecb56e5a7b64018fbcf1
Diwakar Sharma <diwakarsharma4589@gmail.com>
2023-05-26 00:17:03 +0530

    teacherRoutes.js

--- School-Marksheet-Management-MEAN-Stack-App/nodeJsAPI/routes/teacherRoutes.js
+++ School-Marksheet-Management-MEAN-Stack-App/nodeJsAPI/routes/teacherRoutes.js
@@ -1,8 +1,6 @@
-var express = require('express');
-var userController = require('../src/teachers/teacherController');
-const router = express.Router();
 
-router.route('/teacher/login').post(userController.loginUserControllerFn);
+
+router.route('/teacher/login').get(userController.loginUserControllerFn);
 router.route('/teacher/create').post(userController.createUserControllerFn);
 
 module.exports = router;
