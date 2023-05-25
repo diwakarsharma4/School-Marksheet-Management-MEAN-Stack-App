cf6340562750a2536708025800932f45b839203a
Diwakar Sharma <diwakarsharma4589@gmail.com>
2023-05-26 00:15:51 +0530

    marksheetRoutes.js

--- School-Marksheet-Management-MEAN-Stack-App/nodeJsAPI/routes/marksheetRoutes.js
+++ School-Marksheet-Management-MEAN-Stack-App/nodeJsAPI/routes/marksheetRoutes.js
@@ -1,11 +1,9 @@
-var express = require('express');
-var marksheetController = require('../src/marksheets/marksheetController');
 const router = express.Router();
 
-router.route('/marksheet/create').post(marksheetController.createMarksheetControllerFn);
+router.route('/marksheet/create').get(marksheetController.createMarksheetControllerFn);
 router.route('/marksheet/getOne/:id').get(marksheetController.getOneMarksheetControllerFn);
 router.route('/marksheet/getAll').get(marksheetController.getAllMarksheetControllerFn);
-router.route('/marksheet/update/:id').patch(marksheetController.updateMarksheetControllerFn);
-router.route('/marksheet/delete/:id').delete(marksheetController.deleteMarksheetControllerFn);
+router.route('/marksheet/update/:id').get(marksheetController.updateMarksheetControllerFn);
+router.route('/marksheet/delete/:id').get(marksheetController.deleteMarksheetControllerFn);
 
 module.exports = router;
